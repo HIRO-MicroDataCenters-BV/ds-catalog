89e2448f30ac27f35397caa1951e30e8bbc27a5f
renuka-gollu <renuka.gollu@hiro-microdatacenters.nl>
2025-10-29 16:31:42 +0100

    update the test caes

--- server/app/rest_api/routes/tests/test_datasets.py
+++ server/app/rest_api/routes/tests/test_datasets.py
@@ -84,6 +84,7 @@ class TestDatasetsRoutes:
             "oca_uri": oca_uri,
             "shacl_url": shacl_url,
             "ontology_url": ontology_url,
+            "related_data_product": related_data_product,
         }
 
     def test_save_dataset_if_file_not_found(self):
