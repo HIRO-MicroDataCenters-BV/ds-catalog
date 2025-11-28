7adb09b8e6d85d1c7e6935a3fe882aeb9fc4158c
renuka-gollu <renuka.gollu@hiro-microdatacenters.nl>
2025-11-28 14:37:42 +0100

    fixing namespace mismatch

--- server/app/core/usecases.py
+++ server/app/core/usecases.py
@@ -181,7 +181,7 @@ class DatasetsUsecases(BaseUsecases, IDatasetsUsecases):
         dataset.set_attribute(DSPACE.metadataFilename, filename)
 
         # 4 Connector enrichment (extracted to separate method)
-        region = catalog.get_attribute(DSPACE.region) or "hus"
+        region = catalog.get_attribute(DSPACE.region)
         connector_base_url = f"https://ds-connector.{region}.nextgen.hiro-develop.nl"
 
         connector_obj = ConnectorIntegration()
