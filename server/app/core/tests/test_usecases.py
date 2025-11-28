3bf9a4f4a637422d5cc93481050fb14553d30a16
renuka-gollu <renuka.gollu@hiro-microdatacenters.nl>
2025-11-28 15:21:17 +0100

    fixing namespace mismatch

--- server/app/core/tests/test_usecases.py
+++ server/app/core/tests/test_usecases.py
@@ -222,7 +222,6 @@ class TestDatasetsUsecases:
 
         filename = "mmio.tar"
         related_data_product = "disease_xyz"
-        connector_url = "https://ds-connector.Test title.nextgen.hiro-develop.nl"
         mmio_id = "EI2z8E6zYvMF_yvquoUJedWi0rKpQsscPf7JlBgIDoOm"
 
         # Mock repositories
@@ -282,7 +281,9 @@ class TestDatasetsUsecases:
         mock_connector_cls.assert_called_once()  # class created
         (
             mock_con_ins.enrich_distributions_with_connector.assert_awaited_once_with(
-                dataset, related_data_product, connector_url
+                dataset,
+                "disease_xyz",
+                "https://ds-connector.Test title.nextgen.hiro-develop.nl",
             )
         )
 
